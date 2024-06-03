9eff2575402030eebe948d55b1f5fbfe5f6a59ad
Ruban Dzmitry <ruban251219872023@gmail.com>
2024-06-03 17:27:27 +0300

    finish select

--- main.js
+++ main.js
@@ -1,77 +1,136 @@
-// сверить код, разобраться с размером зеленого бакграунда
-// и размером ползунка
-// JS код сравнить
 
-const options = [
-    {
-        label: 'Витебск', value: 2
-    },
-    {
-        label: 'Могилев', value: 6
-    },
-    {
-        label: 'Гомель', value: 3
-    },
-    {
-        label: 'Брест', value: 1
-    },
-    {
-        label: 'Гродно', value: 4
-    },
-    {
-        label: 'Минск', value: 7
-    },
-    {
-        label: 'Москва', value: 777
-    },
+
+const options = [{
+    label: 'Витебск',
+    value: 2,
+    group: 'Витебская область'
+},
+{
+    label: 'Могилев',
+    value: 6,
+    group: 'Первая'
+},
+{
+    label: 'Гомель',
+    value: 3,
+    group: 'Вторая'
+
+},
+{
+    label: 'Брест',
+    value: 1,
+    group: 'Вторая'
+
+},
+{
+    label: 'Гродно',
+    value: 4,
+    group: 'Третья'
+
+},
+{
+    label: 'Минск',
+    value: 7
+},
+{
+    label: 'Москва',
+    value: 777
+},
+{
+    label: 'Орша',
+    value: 123,
+    group: 'Витебская область'
+
+},
 ]
 
 class Select {
-    constructor (selector, options, width) {
-        this.$select = document.querySelector(selector);
-        this.options = options;
-        this.$select.style.setProperty('--selected-width', width);
-        this.$label = document.querySelector('.select__label');
-        this.$dropDown = document.querySelector('.select__drop-down');
-        this.itemsForDropDown = this.options.map(({label, value}) => {
-            return `<li data-id=${value}>${label}</li>`
-        }).join('')
-        this.$dropDown.insertAdjacentHTML('afterbegin', this.itemsForDropDown)
+constructor(selector, options, width) {
+    this.$select = document.querySelector(selector);
+    this.options = options;
+    this.$select.style.setProperty('--selected-width', width);
+    this.$label = document.querySelector('.select__label');
+    this.$dropDown = document.querySelector('.select__drop-down');
 
-        this.$select.addEventListener('click', (e) =>  {
-            console.log(e.target);
-            console.log(e.target.tagName);
-            console.log(e.target.classList);
-            if(e.target.classList.contains('select__label')) {
-                    console.log('Ты кликнул на label');
-            } else {
-                console.log(2);
-                if(e.target.tagName.toLowerCase() === 'li') {
-                        console.log('Ты кликнул на Li');
-                        console.log(e.target.dataset.id);
-                        this.selectItem(+e.target.dataset.id)
-                        // не работает весь if
 
-                }
+    this.itemsForDropDown = this.initGroup(this.options).map(([key, items]) => {
+        if (key) {
+
+            const groupList = items.map(({ label, value }) => {
+                return `<li data-id=${value}>${label}</li>`
+            }).join('')
+
+            return `<ul style="padding: 8px"><span style="color: gray">${key}</span>${groupList}</ul>`
+
+        } else {
+            return items.map(({ label, value }) => {
+                return `<li data-id=${value}>${label}</li>`
+            }).join('')
+        }
+    }).join('')
+
+    this.$dropDown.insertAdjacentHTML('afterbegin', this.itemsForDropDown)
+
+    this.$select.addEventListener('click', (e) => {
+        if (e.target.classList.contains('select__label')) {
+
+    
+
+            this.$select.classList.toggle('active')
+        } else {
+            if (e.target.tagName.toLowerCase() === 'li') {
+                console.log('ты кликнул на ли');
+                this.selectItem(+e.target.dataset.id)
+                this.close()
             }
-        })
-    }
-
-    selectItem() {
-        const selectedElement = this.options.find(item => item.value === id)
-        console.log(selectedElement);
-        this.$label.inneHTML = selectedElement
-    }
+        }
+    })
 }
 
 
-       
+selectItem(id) {
+    const selectedElement = this.options.find(item => item.value === id)
+    this.$label.innerHTML = selectedElement.label
+}
+
+open() {
+    this.$select.classList.add('active')
+}
+
+close() {
+    this.$select.classList.remove('active')
+}
+
+initGroup(items) {
+    const group = new Map()
+    console.log('group', group);
+
+    items.forEach((item) => {
+
+        if (!group.has(item.group)) {
+            group.set(item.group, [item])
+        } else {
+            group.set(item.group, [...group.get(item.group), item])
+        }
+    })
+    return Array.from(group.entries())
+}
 
 
-
-
-
+}
 
 
 const customSelect = new Select('.select', options, '350px')
-console.log(customSelect);
+
+
+
+
+
+
+
+
+
+
+
+
+
